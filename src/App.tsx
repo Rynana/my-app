0a55c51228aaa017cb10f1b158d2b59de7135c5c
Ryan Farrell <ryanmfarrell6@gmail.com>
2025-09-28 19:27:52 +1000

    Updated files: package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  
   return (
     <div>
       <div>
-        <TopSection/>
+        <TopSection />
       </div>
       <div>
-        <MaterialsTable materials={materialsList}/>
+        <MaterialsTable materials={materialsList} />
       </div>
     </div>
   );
@@ -23,73 +22,92 @@ type materialsItem = {
   name: string;
   location: string;
   stock: number;
-}
+};
 
-function AddItemButton(){
-  return(
-    <span>ADD ITEM</span>
+function AddItemButton() {
+  return (
+    <button
+      onClick={() => {
+        alert("clicked");
+      }}
+      style={{ cursor: "pointer", color: "green" }}
+    >
+      Add BEAH
+    </button>
   );
 }
 
-function TopSection(){
-  return(
+function TopSection() {
+  return (
     <div>
-      <div className='main-header'><h1> No Beah? </h1></div>
-      <div><AddItemButton/></div>
+      <div className="main-header">
+        <h1> No Beah? </h1>
+      </div>
+      <div>
+        <AddItemButton />
+      </div>
     </div>
-  )
+  );
 }
 
-function MaterialsTable({ materials }:{materials:materialsItem []}){
-return(
-  <div className='materials-table-container'>
-    <div className='materials-table-header-container'>
-      <div className='header-text'>
-        Category
+function MaterialsTable({ materials }: { materials: materialsItem[] }) {
+  return (
+    <div className="materials-table-container">
+      <div className="materials-table-header-container">
+        <div className="header-text">Category</div>
+        <div className="header-text">Name</div>
+        <div className="header-text">Location</div>
+        <div className="header-text">Price</div>
+        <div className="header-text">Stock</div>
       </div>
-            <div className='header-text'>
-        Name
-      </div>
-            <div className='header-text'>
-        Location
-      </div>
-            <div className='header-text'>
-        Price
-      </div>
-            <div className='header-text'>
-        Stock
+      <div className="materials-table-data">
+        {materials.map((m) => (
+          <div className="materials-table-datarow">
+            <div className="data-text">{m.category}</div>
+            <div className="data-text">{m.name}</div>
+            <div className="data-text">{m.location}</div>
+            <div className="data-text">{m.price}</div>
+            <div className="data-text">{m.stock}</div>
+          </div>
+        ))}
       </div>
     </div>
-    <div className='materials-table-data'>
-      {materials.map((m)=>(
-        <div className='materials-table-datarow'>
-          <div className='data-text'>
-          {m.category}
-          </div>
-          <div className='data-text'>
-          {m.name}
-          </div>
-          <div className='data-text'>
-          {m.location}
-          </div>
-          <div className='data-text'>
-          {m.price}
-          </div>
-          <div className='data-text'>
-          {m.stock}
-          </div>
-        </div>
-      ))}
-    </div>
-  </div>
-)
+  );
 }
 
-let materialsList: materialsItem[]= [
-  {category: "Cabling", quantity: 300, price: 300, name: "Cat6E", location: "Sydney", stock: 4500},
-  {category: "Tower", quantity: 1, price: 350000, name: "LeBlanc Tower", location: "Sydney", stock: 3},
-  {category: "Solar", quantity: 24, price: 6500, name: "Eltek", location: "Gosford", stock: 25},
-  {category: "Bean", quantity: 1, price: 1, name: "Beah", location: "Lewisham", stock: 1}
+let materialsList: materialsItem[] = [
+  {
+    category: "Cabling",
+    quantity: 300,
+    price: 300,
+    name: "Cat6E",
+    location: "Sydney",
+    stock: 4500,
+  },
+  {
+    category: "Tower",
+    quantity: 1,
+    price: 350000,
+    name: "LeBlanc Tower",
+    location: "Sydney",
+    stock: 3,
+  },
+  {
+    category: "Solar",
+    quantity: 24,
+    price: 6500,
+    name: "Eltek",
+    location: "Gosford",
+    stock: 25,
+  },
+  {
+    category: "Bean",
+    quantity: 1,
+    price: 1,
+    name: "Beah",
+    location: "Lewisham",
+    stock: 1,
+  },
 ];
 
 export default App;
